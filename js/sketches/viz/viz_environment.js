a1916975fad954d8e9348daefa617b7a1842b7db
Raizel L <rlagun@uw.edu>
2025-11-26 13:43:05 -0800

    Enviroment Interactive Graph Initial Code

---
+++ js/sketches/viz/viz_environment.js
@@ -0,0 +1,26 @@
+(function () {
+    window.VizEnvironment = {
+        draw: function (p, manager, ai, progress) {
+            var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
+            var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
+            p.push();
+            p.noStroke();
+            p.fill(255);
+            var w = 420;
+            var h = 120;
+            p.rect(cx - w / 2, cy - h / 2, w, h, 6);
+
+            p.fill(0);
+            p.textAlign(p.CENTER, p.CENTER);
+            p.textSize(48);
+            p.text('Visualizing the Road Ahead:', cx, cy);
+
+            p.textSize(42);
+            p.text(' How Everyday Driving Choices', cx, cy + 40);
+            p.text('and Conditions Impact Safety', cx, cy + 80);
+            p.pop();
+        }
+    };
+})();
+
+
